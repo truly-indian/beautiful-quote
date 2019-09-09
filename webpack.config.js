a6795d72d5d35de38502a0ad057836638a297c08
DEEPAK MALIK <deepakm661@gmail.com>
2019-09-09 09:56:32 +0530

    deployed

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,9 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/beautiful-quote/'
+    : '/',
     filename: 'build.js'
   },
   module: {
